bea138e97e6501c77c92f2f7e1ae077b2c00f317
zrz616 <zrz616@gmail.com>
2021-12-05 19:36:41 +0800

    添加merge k sorted lists

---
+++ week3/23.MergekSortedLists.go
@@ -0,0 +1,35 @@
+func mergeKLists(lists []*ListNode) *ListNode {
+	amount := len(lists)
+	interval := 1
+	for interval < amount {
+		for i := 0; i < amount-interval; i += interval * 2 {
+			lists[i] = merge2Lists(lists[i], lists[i+interval])
+		}
+		interval *= 2
+	}
+	if amount > 0 {
+		return lists[0]
+	}
+	return nil
+}
+
+func merge2Lists(l1 *ListNode, l2 *ListNode) *ListNode {
+	head := &ListNode{Val: 0}
+	point := head
+	for l1 != nil && l2 != nil {
+		if l1.Val <= l2.Val {
+			point.Next = l1
+			l1 = l1.Next
+		} else {
+			point.Next = l2
+			l2 = l2.Next
+		}
+		point = point.Next
+	}
+	if l1 == nil {
+		point.Next = l2
+	} else {
+		point.Next = l1
+	}
+	return head.Next
+}
